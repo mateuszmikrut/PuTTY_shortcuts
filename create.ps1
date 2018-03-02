4b74722553983e0b04377042b1a6a2d4f2779b01
Mateusz Mikrut <mateusz.mikrut@gmail.com>
2018-03-02 15:34:16 +0100

    First commit

---
+++ create.ps1
@@ -0,0 +1,18 @@
+﻿$sessions = Get-ChildItem 'HKCU:\Software\SimonTatham\PuTTY\Sessions'
+#$dir = ("{0}\Microsoft\Windows\Start Menu\Programs\ssh" -f $env:APPDATA)
+$dir = ("{0}\ssh_connections" -f $HOME)
+
+rm $dir -Confirm -ErrorAction SilentlyContinue
+mkdir $dir -ErrorAction SilentlyContinue 
+
+foreach ($i in $sessions) {
+    
+    $name       = ($i.Name.Substring($i.Name.LastIndexOf("\") + 1)).Replace("%20"," ")
+        
+    $WshShell = New-Object -comObject WScript.Shell
+    $Shortcut = $WshShell.CreateShortcut(("{0}\ssh_{1}.lnk" -f $dir,$name))
+    $Shortcut.TargetPath = ("{0}\putty\putty.exe" -f $HOME)
+    $Shortcut.Arguments = ("-load {0}" -f $name)
+    $Shortcut.Save()
+}
+
