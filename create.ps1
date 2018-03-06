972e22bfa832113798c6f9028906c98063ade9a6
Mateusz Mikrut <mateusz.mikrut@gmail.com>
2018-03-06 09:24:38 +0100

    Small changes

--- create.ps1
+++ create.ps1
@@ -2,15 +2,17 @@
 #$dir = ("{0}\Microsoft\Windows\Start Menu\Programs\ssh" -f $env:APPDATA)
 $dir = ("{0}\ssh_connections" -f $HOME)
 
-rm $dir -Confirm -ErrorAction SilentlyContinue
-mkdir $dir -ErrorAction SilentlyContinue 
+rm $dir -Recurse -ErrorAction SilentlyContinue | Out-Null
+mkdir $dir -ErrorAction SilentlyContinue  | Out-Null
 
 foreach ($i in $sessions) {
     
-    $name       = ($i.Name.Substring($i.Name.LastIndexOf("\") + 1)).Replace("%20"," ")
+    $name = ($i.Name.Substring($i.Name.LastIndexOf("\") + 1)).Replace("%20"," ")
         
     $WshShell = New-Object -comObject WScript.Shell
-    $Shortcut = $WshShell.CreateShortcut(("{0}\ssh_{1}.lnk" -f $dir,$name))
+    # Commented out - with ssh prefix
+    #$Shortcut = $WshShell.CreateShortcut(("{0}\ssh_{1}.lnk" -f $dir,$name))
+    $Shortcut = $WshShell.CreateShortcut(("{0}\{1}.lnk" -f $dir,$name))
     $Shortcut.TargetPath = ("{0}\putty\putty.exe" -f $HOME)
     $Shortcut.Arguments = ("-load {0}" -f $name)
     $Shortcut.Save()
